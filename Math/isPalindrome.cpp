67fb2d49f19f2c4399558304064b20f037b4386b
Arkesh009 <arkeshbhargava@gmail.com>
2025-03-27 00:37:04 +0530

    isPalindrome

--- Math/isPalindrome.cpp
+++ Math/isPalindrome.cpp
@@ -1,20 +1,41 @@
 // 9. Given an integer x, return true if x is a palindrome, and false otherwise
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-bool isPalindrome(int x) {
-        
+bool isPalindrome(int x)
+{
+
+    if (x < 0 || (x % 10 == 0 && x != 0))
+    {
+        return false;
+    }
+
+    int reverse = 0;
+    int temp = x;
+    while (x)
+    {
+        int last = x % 10;
+        reverse = reverse * 10 + last;
+        x /= 10;
+    }
+
+    return temp == reverse;
 }
 
 int main()
 {
-   int x;
-   cin >> x;
+    int x;
+    cin >> x;
 
-   if(isPalindrome(x)){
+    if (isPalindrome(x))
+    {
         cout << "The integer is a Palindrome\n";
-   }
-   
-   return 0;
+    }
+    else
+    {
+        cout << "Its not\n";
+    }
+
+    return 0;
 }
