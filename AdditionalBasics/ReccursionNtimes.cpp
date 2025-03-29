15424fceb85c1a0b1ef17a1b40ff2a18c131aa9f
Arkesh009 <arkeshbhargava@gmail.com>
2025-03-29 23:32:26 +0530

    reccrsion

--- AdditionalBasics/ReccursionNtimes.cpp
+++ AdditionalBasics/ReccursionNtimes.cpp
@@ -3,8 +3,25 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+void nPrints(int, char);
+
 int main()
 {
+    char a;
+    int x;
+    cout << "Enter the char you wanna print\n";
+    cin >> a;
+    cout << "Enter the number of times you wanna print it\n";
+    cin >> x;
+    nPrints(x, a);
     
    return 0;
+}
+
+void nPrints(int x, char a){
+    if(x==0){
+        return;
+    }
+    cout << a << " ";
+    nPrints(x-1, a);
 }
