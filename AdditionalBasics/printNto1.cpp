bc0d0ed4e3edea00b3b409d3b5d8702792110d98
Arkesh009 <arkeshbhargava@gmail.com>
2025-03-30 20:51:30 +0530

    printN-1

---
+++ AdditionalBasics/printNto1.cpp
@@ -0,0 +1,25 @@
+// print N to 1 in 
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void printN(int);
+
+int main()
+{
+    int n;
+    cout << "Enter the cout\n";
+    cin >> n;
+    printN(n);
+
+    return 0;
+}
+
+void printN(int n)
+{
+    if (n>0)
+    {
+        cout << n << " ";
+    }
+    printN(n-1);
+}
