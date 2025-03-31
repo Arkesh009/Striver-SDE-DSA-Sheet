4e62bd17a9447a2b034755cf2547bb01ac4148bd
Arkesh009 <arkeshbhargava@gmail.com>
2025-03-31 23:24:17 +0530

    fact

--- AdditionalBasics/Reccursion/factorialReccursion.cpp
+++ AdditionalBasics/Reccursion/factorialReccursion.cpp
@@ -11,16 +11,13 @@ int main()
     cout << "Enter the number of terms\n";
     cin >> n;
     cout << "Factorial: " << factN(n);
-
     return 0;
 }
 
 int factN(int n)
 {
-    if (n == 1)
-    {
+    if (n == 1){
         return 1;
     }
-
     return n * factN(n - 1);
 }
