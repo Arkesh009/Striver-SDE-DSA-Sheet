2f111e2cc548a644b1e950d00def4f54364a594e
Arkesh009 <arkeshbhargava@gmail.com>
2025-04-01 22:37:58 +0530

    hash

--- AdditionalBasics/Hashing/basicHashing.cpp
+++ AdditionalBasics/Hashing/basicHashing.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
@@ -8,13 +8,15 @@ int main()
     cin >> n;
     int arr[n];
     cout << "Enter the elements in the array\n";
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         cin >> arr[i];
     }
 
     // hashing
-    int hash[20]={0};
-    for(int i=0; i<n; i++){
+    int hash[20] = {0};
+    for (int i = 0; i < n; i++)
+    {
         hash[arr[i]] += 1;
     }
 
@@ -22,13 +24,14 @@ int main()
     int q;
     cout << "Enter the number of querries\n";
     cin >> q;
-    while (q--) {
+    while (q--)
+    {
         int number;
         cout << "Enter the num\n";
         cin >> number;
         // fetching:
         cout << hash[number] << endl;
     }
-   
-   return 0;
+
+    return 0;
 }
