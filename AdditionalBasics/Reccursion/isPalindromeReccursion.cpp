e47dec79d176b4d13111420b0456d0630a1dd0bd
Arkesh009 <arkeshbhargava@gmail.com>
2025-04-01 23:52:29 +0530

    palindrome

--- AdditionalBasics/Reccursion/isPalindromeReccursion.cpp
+++ AdditionalBasics/Reccursion/isPalindromeReccursion.cpp
@@ -1,6 +1,6 @@
 // Check if the given string is a Palindrome using reccursive technique
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 bool isPalindrome(string s, int);
@@ -10,26 +10,32 @@ int main()
     string s;
     cout << "Enter the string\n";
     getline(cin, s);
-    
-    if(isPalindrome(s,0)){
+
+    if (isPalindrome(s, 0))
+    {
         cout << "It is a Palindrome\n";
-    }else{
+    }
+    else
+    {
         cout << "Not a Palindrome\n";
     }
     isalnum(s[0]);
-   
-   return 0;
+
+    return 0;
 }
 
-bool isPalindrome(string s, int i){
+bool isPalindrome(string s, int i)
+{
 
-    if(i >= s.length()/2){
+    if (i >= s.length() / 2)
+    {
         return true;
     }
 
-    if(s[i] != s[s.length()-i-1]){
+    if (s[i] != s[s.length() - i - 1])
+    {
         return false;
     }
 
-    return isPalindrome(s, i+1);
+    return isPalindrome(s, i + 1);
 }
