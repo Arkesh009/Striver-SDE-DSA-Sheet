0f990c7afb8916bbc5de4c953e8dabbc17e5aa8b
Arkesh009 <arkeshbhargava@gmail.com>
2025-04-02 22:34:16 +0530

    return divisor

--- Math/returnAllDivisors.cpp
+++ Math/returnAllDivisors.cpp
@@ -1,27 +1,32 @@
 // Print all the divisors upto n
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-void divisors(int n){
+void divisors(int n)
+{
     vector<int> v;
     // O(sqrt(n))
-    for(int i=1; i*i<=n; i++){
-        if(n%i == 0){
+    for (int i = 1; i * i <= n; i++)
+    {
+        if (n % i == 0)
+        {
             v.push_back(i);
-            if(n/i != i){ // adding counter part to the vec ex: 14/1 = 14, 14/2 = 7 etc.
-                v.push_back(n/i);
+            if (n / i != i)
+            { // adding counter part to the vec ex: 14/1 = 14, 14/2 = 7 etc.
+                v.push_back(n / i);
             }
         }
     }
     sort(v.begin(), v.end());
-    for(auto i: v){
+    for (auto i : v)
+    {
         cout << i << " ";
     }
 }
 int main()
 {
-   int n;
-   cin >> n;
+    int n;
+    cin >> n;
     divisors(n);
-   return 0;
+    return 0;
 }
