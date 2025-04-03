6129e19860c2774a964bb4bf2a4daf1f76641229
Arkesh009 <arkeshbhargava@gmail.com>
2025-04-03 22:09:34 +0530

    char

--- AdditionalBasics/Hashing/charHashingBothLCandUC.cpp
+++ AdditionalBasics/Hashing/charHashingBothLCandUC.cpp
@@ -1,30 +1,32 @@
 // Character hassing if the string contains both upper and lowercase
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-   string s;
-   cout << "Enter the string\n";
-   getline(cin,s);
+     string s;
+     cout << "Enter the string\n";
+     getline(cin, s);
 
-   // Precompute
-   int hash[256] = {0};
-   for(int i=0; i<s.size(); i++){
-        hash[s[i]]++;
-   }
+     // Precompute
+     int hash[256] = {0};
+     for (int i = 0; i < s.size(); i++)
+     {
+          hash[s[i]]++;
+     }
 
-   // querries
-   int q;
-   cout << "Enter the number of querries\n";
-   cin >> q;
-   while(q--){
-        char c;
-        cin >> c;
-        // fetch
-        cout << hash[c] << endl;
-   }
-   
-   return 0;
+     // querries
+     int q;
+     cout << "Enter the number of querries\n";
+     cin >> q;
+     while (q--)
+     {
+          char c;
+          cin >> c;
+          // fetch
+          cout << hash[c] << endl;
+     }
+
+     return 0;
 }
