c076e09eefaf3460c693314e1f3eb767e600872d
Arkesh009 <arkeshbhargava@gmail.com>
2025-04-05 22:58:20 +0530

    05

---
+++ AdditionalBasics/Patterns/05_pattern.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void seeding(int n) {
+	 
+	for(int i=0; i<n; i++){
+		for(int j=n; j>i; j--){
+			cout << "* ";
+		}
+		cout << "\n";
+	}
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the side length of seeding\n";
+    cin >> n;
+    cout << "The derised Pattern\n";
+    seeding(n);
+   
+   return 0;
+}
