1e616409a2176f853f0aabf620a66cbbd164da77
Arkesh009 <arkeshbhargava@gmail.com>
2025-04-07 01:01:13 +0530

    11

---
+++ AdditionalBasics/Patterns/11_pattern.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void nBinaryTriangle(int n) {
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=i; j++){
+            if((i+j)%2 == 0){
+                cout << "1 ";
+            }else{
+                cout << "0 ";
+            }
+        }
+        cout << "\n";
+    }
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the length of B-Triangle\n";
+    cin >> n;
+    cout << "The derised Pattern\n";
+    nBinaryTriangle(n);
+   return 0;
+}
