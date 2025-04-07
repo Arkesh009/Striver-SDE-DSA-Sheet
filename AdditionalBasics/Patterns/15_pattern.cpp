f7a12bd3a3c1fdaf5a320d48d6be1543d8a3f9e1
Arkesh009 <arkeshbhargava@gmail.com>
2025-04-07 22:10:50 +0530

    15

---
+++ AdditionalBasics/Patterns/15_pattern.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void nLetterTriangle(int n) {
+    
+    for(int i=1; i<=n; i++){
+        char ch = 'A';
+        for(int j=n; j>=i; j--){
+            cout << ch << " ";
+            ch++;
+        }
+        cout << "\n";
+    }
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the length of Letter Triangle\n";
+    cin >> n;
+    cout << "The derised Pattern\n";
+    nLetterTriangle(n);
+   return 0;
+}
