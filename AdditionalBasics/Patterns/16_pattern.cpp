796c9746735f822d3ccb12789fa749357b4827ec
Arkesh009 <arkeshbhargava@gmail.com>
2025-04-07 22:15:13 +0530

    16

---
+++ AdditionalBasics/Patterns/16_pattern.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void alphaRamp(int n) {
+    char ch = 'A';
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=i; j++){
+            cout << ch << " ";
+        }
+        ch++;
+        cout << "\n";
+    }
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the length of Alpha Ramp\n";
+    cin >> n;
+    cout << "The derised Pattern\n";
+    alphaRamp(n);
+   return 0;
+}
