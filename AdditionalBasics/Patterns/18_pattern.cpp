7090c7e5caa2237763ae9b77bd5ee4f4a49ca0d3
Arkesh009 <arkeshbhargava@gmail.com>
2025-04-08 22:34:28 +0530

    18

---
+++ AdditionalBasics/Patterns/18_pattern.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void alphaTriangle(int n) {
+    for(int i=1; i<=n; i++){
+        char ch = 'A'+n-1;
+        for(int j=1; j<=i; j++){
+            cout << ch << " ";
+            ch--;
+        }
+        cout << "\n";
+    }
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the length of Alpha Triangle\n";
+    cin >> n;
+    cout << "The derised Pattern\n";
+    alphaTriangle(n);
+   return 0;
+}
