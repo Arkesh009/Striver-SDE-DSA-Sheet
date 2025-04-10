05ecde684a7b8e8e96b0107b9514c83f247bc326
Arkesh009 <arkeshbhargava@gmail.com>
2025-04-10 21:36:21 +0530

    22

--- AdditionalBasics/Patterns/22_pattern.cpp
+++ AdditionalBasics/Patterns/22_pattern.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 void getNumberPattern(int n) {
     int size = 2*n-1;
+    
     for(int i=0; i<size; i++){
         for(int j=0; j<size; j++){
             cout << n - min({i, j, size-1-i, size-1-j});
