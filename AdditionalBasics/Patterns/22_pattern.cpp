1dcdaf0edee36175d90adc228f4f4b7c9353df11
Arkesh009 <arkeshbhargava@gmail.com>
2025-04-10 21:35:51 +0530

    21

--- AdditionalBasics/Patterns/22_pattern.cpp
+++ AdditionalBasics/Patterns/22_pattern.cpp
@@ -1,8 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+void getNumberPattern(int n) {
+    int size = 2*n-1;
+    for(int i=0; i<size; i++){
+        for(int j=0; j<size; j++){
+            cout << n - min({i, j, size-1-i, size-1-j});
+        }
+        cout << "\n";
+    }
+}
+
 int main()
 {
-   
+    int n;
+    cout << "Enter the value of n: ";
+    cin >> n;
+    cout << "The derised Pattern\n";
+    getNumberPattern(n);
    return 0;
 }
