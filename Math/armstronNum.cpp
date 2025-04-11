d2fb122d8434f7e556239197d77a591f9c476964
Arkesh009 <arkeshbhargava@gmail.com>
2025-04-11 22:25:08 +0530

    Armstrong Num

---
+++ Math/armstronNum.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isArmstrong(int n) {
+    if(n<0){
+        return false;
+    }
+
+    int temp = n;
+    int sum = 0;
+    int digits = 0;
+    while(temp){
+        digits++;
+        temp /= 10;
+    }
+
+    temp = n;
+
+    while(temp){
+        int last = temp%10;
+        sum += pow(last, digits);
+        temp /= 10;
+    }
+
+    return sum == n;
+}
+
+int main()
+{
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    if (isArmstrong(n)) {
+        cout << n << " is an Armstrong number." << endl;
+    } else {
+        cout << n << " is not an Armstrong number." << endl;
+    }
+
+   return 0;
+}
