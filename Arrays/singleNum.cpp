86dd44f98ad86d922b7b2c8db8bd38e1df82f5ed
Arkesh009 <arkeshbhargava@gmail.com>
2025-04-12 22:57:08 +0530

    single num

--- Arrays/singleNum.cpp
+++ Arrays/singleNum.cpp
@@ -1,16 +1,18 @@
 // 136. Single Number
 // Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
-//You must implement a solution with a linear runtime complexity and use only constant extra space.
+// You must implement a solution with a linear runtime complexity and use only constant extra space.
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int singleNumber(vector<int>& nums) {
+int singleNumber(vector<int> &nums)
+{
     int result = 0;
-    for(auto i: nums){
+    for (auto i : nums)
+    {
         result ^= i;
     }
-return result;
+    return result;
 }
 
 int main()
@@ -19,21 +21,22 @@ int main()
     // Input array size
     int n;
     cin >> n;
- 
+
     // Input the array elements
     cout << "Enter the elements of the array\n";
     vector<int> nums;
-    for(int i=0; i<n; i++){
-         int ele;
-         cin >> ele;
-         nums.push_back(ele);
+    for (int i = 0; i < n; i++)
+    {
+        int ele;
+        cin >> ele;
+        nums.push_back(ele);
     }
- 
+
     // Call the singleNumber function
     int result = singleNumber(nums);
- 
+
     // Output the result
     cout << "The single number is: " << result << endl;
-    
-   return 0;
+
+    return 0;
 }
