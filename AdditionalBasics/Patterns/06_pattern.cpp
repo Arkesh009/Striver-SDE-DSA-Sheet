cc44ccbbac6a0d198ae6f161edb12ae4d53607a2
Arkesh009 <arkeshbhargava@gmail.com>
2025-04-13 23:35:31 +0530

    06

--- AdditionalBasics/Patterns/06_pattern.cpp
+++ AdditionalBasics/Patterns/06_pattern.cpp
@@ -1,10 +1,13 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-void nNumberTriangle(int n) {
-    
-    for(int i=n; i>0; i--){
-        for(int j=1; j<=i; j++){
+void nNumberTriangle(int n)
+{
+
+    for (int i = n; i > 0; i--)
+    {
+        for (int j = 1; j <= i; j++)
+        {
             cout << j << " ";
         }
         cout << "\n";
@@ -18,5 +21,5 @@ int main()
     cin >> n;
     cout << "The derised Pattern\n";
     nNumberTriangle(n);
-   return 0;
+    return 0;
 }
