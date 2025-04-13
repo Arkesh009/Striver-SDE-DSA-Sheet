59ac973dfe362820c4610ee17100b99c29897b6f
Arkesh009 <arkeshbhargava@gmail.com>
2025-04-13 23:33:59 +0530

    01

--- AdditionalBasics/Patterns/01_pattern.cpp
+++ AdditionalBasics/Patterns/01_pattern.cpp
@@ -1,10 +1,13 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-void nForest(int n) {
+void nForest(int n)
+{
 	// Write your code here.
-	for(int i=0; i<n; i++){
-		for(int j=0; j<n; j++){
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
 			cout << "* ";
 		}
 		cout << "\n";
@@ -13,11 +16,11 @@ void nForest(int n) {
 
 int main()
 {
-   int n;
-   cout << "Enter the side length of matrix\n";
-   cin >> n;
-   cout << "The derised Pattern\n";
-   nForest(n);
+	int n;
+	cout << "Enter the side length of matrix\n";
+	cin >> n;
+	cout << "The derised Pattern\n";
+	nForest(n);
 
-   return 0;
+	return 0;
 }
