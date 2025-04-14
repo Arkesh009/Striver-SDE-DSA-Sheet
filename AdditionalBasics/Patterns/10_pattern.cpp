d5d34b3b60f4494f1aa5c89fc32f7623ba3b33bc
Arkesh009 <arkeshbhargava@gmail.com>
2025-04-14 23:39:45 +0530

    10

--- AdditionalBasics/Patterns/10_pattern.cpp
+++ AdditionalBasics/Patterns/10_pattern.cpp
@@ -1,22 +1,26 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-void nStarTriangle(int n) {
-    int a=0;
-    for(int i=0; i<n; i++){
-        for(int j=0; j<=i; j++){
+void nStarTriangle(int n)
+{
+    int a = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
             cout << "*";
         }
         cout << "\n";
     }
-    
-    for(int i=n-1; i>0; i--){
-        for(int j=0; j<i; j++){
+
+    for (int i = n - 1; i > 0; i--)
+    {
+        for (int j = 0; j < i; j++)
+        {
             cout << "*";
         }
         cout << "\n";
     }
-    
 }
 
 int main()
@@ -26,5 +30,5 @@ int main()
     cin >> n;
     cout << "The derised Pattern\n";
     nStarTriangle(n);
-   return 0;
+    return 0;
 }
