4776f623ab3c2b649a5eed04c78610ea15e989ee
Arkesh009 <arkeshbhargava@gmail.com>
2025-04-15 23:29:57 +0530

    16

--- AdditionalBasics/Patterns/16_pattern.cpp
+++ AdditionalBasics/Patterns/16_pattern.cpp
@@ -1,10 +1,13 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-void alphaRamp(int n) {
+void alphaRamp(int n)
+{
     char ch = 'A';
-    for(int i=1; i<=n; i++){
-        for(int j=1; j<=i; j++){
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
             cout << ch << " ";
         }
         ch++;
@@ -19,5 +22,5 @@ int main()
     cin >> n;
     cout << "The derised Pattern\n";
     alphaRamp(n);
-   return 0;
+    return 0;
 }
