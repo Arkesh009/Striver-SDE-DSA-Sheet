ed3d82cf695531bf45f04a5a9fde78c120684359
Arkesh009 <arkeshbhargava@gmail.com>
2025-04-15 23:30:19 +0530

    17

--- AdditionalBasics/Patterns/17_pattern.cpp
+++ AdditionalBasics/Patterns/17_pattern.cpp
@@ -1,18 +1,24 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-void alphaHill(int n) {
-    for(int i=1; i<=n; i++){
-        for(int j=n-1; j>=i; j--){
+void alphaHill(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = n - 1; j >= i; j--)
+        {
             cout << " ";
         }
         char ch = 'A';
-        for(int j=1; j<=2*i-1; j++){
+        for (int j = 1; j <= 2 * i - 1; j++)
+        {
             cout << ch << " ";
-            if(i>j){
+            if (i > j)
+            {
                 ch++;
             }
-            else{
+            else
+            {
                 ch--;
             }
         }
@@ -27,5 +33,5 @@ int main()
     cin >> n;
     cout << "The derised Pattern\n";
     alphaHill(n);
-   return 0;
+    return 0;
 }
