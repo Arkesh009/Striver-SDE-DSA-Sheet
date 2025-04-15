0aac7b89ba7f72e2d25bf3245061e0811e08497c
Arkesh009 <arkeshbhargava@gmail.com>
2025-04-15 21:25:09 +0530

    Selection sort

--- Sorting/selectionSort.cpp
+++ Sorting/selectionSort.cpp
@@ -1,39 +1,51 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-vector<int> selectionSort(vector<int>& nums) {
-    for(int i=0; i<nums.size()-1; i++){
-        int min = i;
-        for(int j=i; j<nums.size(); j++){
-            if(nums[min] > nums[j]){
-                min = j;
+vector<int> selectionSort(vector<int> &nums)
+{
+    for (int i = 0; i < nums.size() - 1; i++)
+    {
+        int min = i; // initially setting minimum value index to i
+        for (int j = i; j < nums.size(); j++)
+        {
+            // checking if the jth index have lesser value
+            if (nums[min] > nums[j])
+            {
+                min = j; // setting minimum vlaue index to j
             }
         }
+        // swapping the ith index with the minimum value index
         swap(nums[i], nums[min]);
     }
+    // return sorted vector
     return nums;
 }
 
 int main()
 {
-   cout << "Enter the size of the array\n";
-   int n;
-   cin >> n;
+    cout << "Enter the size of the array\n";
+    int n;
+    // Array size
+    cin >> n;
 
-   cout << "Enter the elements in the array\n";
-   vector<int> nums;
-   for(int i=0; i<n; i++){
+    cout << "Enter the elements in the array\n";
+    vector<int> nums;
+    // Array input
+    for (int i = 0; i < n; i++)
+    {
         int ele;
         cin >> ele;
         nums.push_back(ele);
-   }
+    }
 
-   selectionSort(nums);
-   
-   cout << "The sorted array = ";
-   for(auto i: nums){
+    selectionSort(nums); // calling function
+
+    cout << "The sorted array = ";
+    // Printing sorted array
+    for (auto i : nums)
+    {
         cout << i << " ";
-   }
+    }
 
-   return 0;
+    return 0;
 }
