1f47cd75518630336b10eedf389096f42d51012c
Arkesh009 <arkeshbhargava@gmail.com>
2025-04-16 22:55:06 +0530

    19

--- AdditionalBasics/Patterns/19_pattern.cpp
+++ AdditionalBasics/Patterns/19_pattern.cpp
@@ -1,28 +1,37 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-void symmetry(int n) {
-    for(int i=0; i<n; i++){
-        for(int j=i; j<n; j++){
+void symmetry(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i; j < n; j++)
+        {
             cout << "* ";
         }
-        for(int j=0; j<2*i; j++){
+        for (int j = 0; j < 2 * i; j++)
+        {
             cout << "  ";
         }
-        for(int j=i; j<n; j++){
+        for (int j = i; j < n; j++)
+        {
             cout << "* ";
         }
         cout << "\n";
     }
 
-    for(int i=1; i<=n; i++){
-        for(int j=0; j<i; j++){
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
             cout << "* ";
         }
-        for(int j=2*(n-i); j>0; j--){
+        for (int j = 2 * (n - i); j > 0; j--)
+        {
             cout << "  ";
         }
-        for(int j=0; j<i; j++){
+        for (int j = 0; j < i; j++)
+        {
             cout << "* ";
         }
         cout << "\n";
@@ -36,5 +45,5 @@ int main()
     cin >> n;
     cout << "The derised Pattern\n";
     symmetry(n);
-   return 0;
+    return 0;
 }
