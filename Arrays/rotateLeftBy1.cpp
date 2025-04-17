64e396d47cbe8e634bde9d8ee1b494bd395837ed
Arkesh009 <arkeshbhargava@gmail.com>
2025-04-17 23:30:33 +0530

    rotate left by 1

--- Arrays/rotateLeftBy1.cpp
+++ Arrays/rotateLeftBy1.cpp
@@ -1,35 +1,38 @@
 // Left rotate an array by one place
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-vector<int> rotateArray(vector<int>& arr, int n) {
-    // ex: arr[] = {1,2,3,4,5}
-    reverse(arr.begin(), arr.end()); // {5,4,3,2,1}
-    reverse(arr.begin(), arr.end()-1); // {2,3,4,5,1}
+vector<int> rotateArray(vector<int> &arr, int n)
+{
+     // ex: arr[] = {1,2,3,4,5}
+     reverse(arr.begin(), arr.end());     // {5,4,3,2,1}
+     reverse(arr.begin(), arr.end() - 1); // {2,3,4,5,1}
 
-    return arr;
+     return arr;
 }
 
 int main()
 {
-    int n;
-    cout << "Enter the size of the array\n";
-    cin >> n;
-   vector<int> arr(n);
-   cout << "Enter the elements of the array\n";
-   for(int i=0; i<n; i++){
-        int ele;
-        cin >> ele;
-        arr[i] = ele;
-   }
-   rotateArray(arr, n);
+     int n;
+     cout << "Enter the size of the array\n";
+     cin >> n;
+     vector<int> arr(n);
+     cout << "Enter the elements of the array\n";
+     for (int i = 0; i < n; i++)
+     {
+          int ele;
+          cin >> ele;
+          arr[i] = ele;
+     }
+     rotateArray(arr, n);
 
-   cout << "The rotated array is:\n{";
-   for(auto i: arr){
-        cout << i << ",";
-   }
-   cout << "}";
+     cout << "The rotated array is:\n{";
+     for (auto i : arr)
+     {
+          cout << i << ",";
+     }
+     cout << "}";
 
-   return 0;
+     return 0;
 }
