a3945b5a8d9ed15a13d91f5a9750c97ca76b6317
Arkesh009 <arkeshbhargava@gmail.com>
2025-04-17 23:29:43 +0530

    max consiqutive 1's

--- Arrays/maxConsiqutive1s.cpp
+++ Arrays/maxConsiqutive1s.cpp
@@ -1,22 +1,28 @@
 // Maximum Consiqutive 1s
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int findMaxConsecutiveOnes(vector<int>& nums) {
+int findMaxConsecutiveOnes(vector<int> &nums)
+{
     int cnt = 0;
     int max = 0;
-    for(auto i: nums){
-        if(i==0){
-            cnt = 0;
-        }else{
-            cnt++;
+    for (auto i : nums)
+    {
+        if (i == 0)
+        {
+            cnt = 0; // setting count to 0 whenever 0 is encountered
         }
-        if(max < cnt){
-            max = cnt;
+        else
+        {
+            cnt++; // incrementing the counter whenever 1 is encountered
         }
-    
+        if (max < cnt)
+        {
+            max = cnt; // setting max to count with each itreation
+        }
     }
+    //  returning max
     return max;
 }
 
@@ -27,13 +33,14 @@ int main()
     cin >> n;
     vector<int> nums(n);
     cout << "Enter the series 1's and 0's inside the array\n";
-    for(int i=0; i<n; i++){
+    for (int i = 0; i < n; i++)
+    {
         int ele;
         cin >> ele;
         nums[i] = ele;
     }
-
+    // Output
     cout << "The maximum number of consiqutive 1's are: " << findMaxConsecutiveOnes(nums);
-   
-   return 0;
+
+    return 0;
 }
