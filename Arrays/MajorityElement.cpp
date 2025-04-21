3a70a5adc4bcdcf4dda34cac6a09748e73df977b
Arkesh009 <arkeshbhargava@gmail.com>
2025-04-21 22:40:53 +0530

    leetcode 169. Majority element

--- Arrays/MajorityElement.cpp
+++ Arrays/MajorityElement.cpp
@@ -1,22 +1,26 @@
 // 169. Maority Element( >n/2)
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int majorityElement(vector<int>& nums) {
+int majorityElement(vector<int> &nums)
+{
     int size = nums.size(); // calculate the size of vector
     unordered_map<int, int>
         hsh; // declaration of a hash map
              // Using an unordered_map<int, int> (hsh) to count the
              // occurrences of each integer in the vector.
-    for (int i : nums) {
+    for (int i : nums)
+    {
         hsh[i]++; // for each int in nums incrementing its count in hash map
     }
 
     int max = 0;
-    for (auto i : hsh) {
+    for (auto i : hsh)
+    {
         // check if the count of the ith key is > size/2
-        if (i.second > size / 2) {
+        if (i.second > size / 2)
+        {
             max = i.first; // updating the max
             break;
         }
@@ -29,17 +33,20 @@ int main()
 {
     int n;
     cout << "Enter the size of the vector\n";
+    // Array size
     cin >> n;
     cout << "Enter the elements of the vector\n";
-   vector<int> nums;
-   for(int i=0; i<n; i++){
+    vector<int> nums;
+    // Elements input
+    for (int i = 0; i < n; i++)
+    {
         int ele;
         cin >> ele;
         nums.push_back(ele);
-   }
+    }
 
-   cout << "The maority element: ";
-   cout << majorityElement(nums);
-   
-   return 0; 
+    cout << "The maority element: ";
+    cout << majorityElement(nums); // function call
+
+    return 0;
 }
