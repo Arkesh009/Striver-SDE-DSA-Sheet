a34ff856dac009358067e16a358c7d0146bcf8ad
Arkesh009 <arkeshbhargava@gmail.com>
2025-04-21 22:11:05 +0530

    leetcode 169

---
+++ Arrays/MajorityElement.cpp
@@ -0,0 +1,45 @@
+// 169. Maority Element( >n/2)
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int majorityElement(vector<int>& nums) {
+    int size = nums.size(); // calculate the size of vector
+    unordered_map<int, int>
+        hsh; // declaration of a hash map
+             // Using an unordered_map<int, int> (hsh) to count the
+             // occurrences of each integer in the vector.
+    for (int i : nums) {
+        hsh[i]++; // for each int in nums incrementing its count in hash map
+    }
+
+    int max = 0;
+    for (auto i : hsh) {
+        // check if the count of the ith key is > size/2
+        if (i.second > size / 2) {
+            max = i.first; // updating the max
+            break;
+        }
+    }
+    // returning the key with maximum occurance
+    return max;
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the size of the vector\n";
+    cin >> n;
+    cout << "Enter the elements of the vector\n";
+   vector<int> nums;
+   for(int i=0; i<n; i++){
+        int ele;
+        cin >> ele;
+        nums.push_back(ele);
+   }
+
+   cout << "The maority element: ";
+   cout << majorityElement(nums);
+   
+   return 0; 
+}
