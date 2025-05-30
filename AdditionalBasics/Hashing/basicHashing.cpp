54de26ce694b448ed86cfe9ae94ca5870ea236d2
Arkesh009 <arkeshbhargava@gmail.com>
2025-05-30 12:00:00 +0530

    basic hash

--- AdditionalBasics/Hashing/basicHashing.cpp
+++ AdditionalBasics/Hashing/basicHashing.cpp
@@ -1,37 +1,39 @@
-#include <bits/stdc++.h>
+#include <bits/stdc++.h> // Include all standard libraries
 using namespace std;
 
 int main()
 {
     int n;
     cout << "Enter the size of array\n";
-    cin >> n;
+    cin >> n; // Input the size of the array
+
     int arr[n];
     cout << "Enter the elements in the array\n";
+    // Input the elements of the array
     for (int i = 0; i < n; i++)
     {
         cin >> arr[i];
     }
 
-    // hashing
-    int hash[20] = {0};
+    // Hashing
+    int hash[20] = {0}; // Initialize a hash array of size 20 with all elements set to 0
     for (int i = 0; i < n; i++)
     {
-        hash[arr[i]] += 1;
+        hash[arr[i]] += 1; // Increment the count of the element in the hash array
     }
 
-    // Handling querry
+    // Handling queries
     int q;
-    cout << "Enter the number of querries\n";
-    cin >> q;
+    cout << "Enter the number of queries\n";
+    cin >> q; // Input the number of queries
     while (q--)
     {
         int number;
         cout << "Enter the num\n";
-        cin >> number;
-        // fetching:
-        cout << hash[number] << endl;
+        cin >> number; // Input the number to query
+        // Fetching:
+        cout << hash[number] << endl; // Output the frequency of the queried number
     }
 
-    return 0;
+    return 0; // Exit the program
 }
