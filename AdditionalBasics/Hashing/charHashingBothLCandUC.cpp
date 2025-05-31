9ae2504b4fb43b860ccc32a3b3493fae27daedaa
Arkesh009 <arkeshbhargava@gmail.com>
2025-05-31 12:00:00 +0530

    char hash

--- AdditionalBasics/Hashing/charHashingBothLCandUC.cpp
+++ AdditionalBasics/Hashing/charHashingBothLCandUC.cpp
@@ -1,4 +1,4 @@
-// Character hassing if the string contains both upper and lowercase
+// Character hashing if the string contains both upper and lowercase
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -7,26 +7,26 @@ int main()
 {
      string s;
      cout << "Enter the string\n";
-     getline(cin, s);
+     getline(cin, s); // Input the string (can include spaces)
 
-     // Precompute
+     // Precompute: Initialize a hash array to store frequency of each character (ASCII 0-255)
      int hash[256] = {0};
      for (int i = 0; i < s.size(); i++)
      {
-          hash[s[i]]++;
+          hash[s[i]]++; // Increment the count for the ASCII value of the character
      }
 
-     // querries
+     // Queries
      int q;
-     cout << "Enter the number of querries\n";
-     cin >> q;
+     cout << "Enter the number of queries\n";
+     cin >> q; // Input the number of queries
      while (q--)
      {
           char c;
-          cin >> c;
-          // fetch
+          cin >> c; // Input the character to query
+          // Fetch and print the frequency of the queried character
           cout << hash[c] << endl;
      }
 
-     return 0;
+     return 0; // Exit the program
 }
