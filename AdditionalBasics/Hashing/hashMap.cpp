dd06e83a04f7f7f47578cadc046c3017256f6896
Arkesh009 <arkeshbhargava@gmail.com>
2025-06-02 12:00:00 +0530

    hashing with map

--- AdditionalBasics/Hashing/hashMap.cpp
+++ AdditionalBasics/Hashing/hashMap.cpp
@@ -7,33 +7,35 @@ int main()
 {
     int n;
     cout << "Enter the size of array\n";
-    cin >> n;
+    cin >> n; // Input the size of the array
+
     int arr[n];
     cout << "Enter the elements in the array\n";
+    // Input the elements of the array
     for (int i = 0; i < n; i++)
     {
         cin >> arr[i];
     }
 
-    // hashing
-    map<int, int> hash;
+    // Hashing
+    map<int, int> hash; // Declare a map to store frequency of each element
     for (int i = 0; i < n; i++)
     {
-        hash[arr[i]]++;
+        hash[arr[i]]++; // Increment the count for each element in the map
     }
 
-    // Handling querry
+    // Handling queries
     int q;
     cout << "Enter the number of querries\n";
-    cin >> q;
+    cin >> q; // Input the number of queries
     while (q--)
     {
         int number;
         cout << "Enter the num\n";
-        cin >> number;
-        // fetching:
+        cin >> number; // Input the number to query
+        // Fetch and print the frequency of the queried number
         cout << hash[number] << endl;
     }
 
-    return 0;
+    return 0; // Exit the program
 }
