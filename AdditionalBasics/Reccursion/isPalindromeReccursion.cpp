f064c5f75dbba8556e4cd766ef7011451612b6d5
Arkesh009 <arkeshbhargava@gmail.com>
2025-06-06 12:00:00 +0530

    palindrome

--- AdditionalBasics/Reccursion/isPalindromeReccursion.cpp
+++ AdditionalBasics/Reccursion/isPalindromeReccursion.cpp
@@ -1,16 +1,18 @@
-// Check if the given string is a Palindrome using reccursive technique
+// Check if the given string is a Palindrome using recursive technique
 
 #include <bits/stdc++.h>
 using namespace std;
 
+// Function declaration: checks if string s is a palindrome starting from index i
 bool isPalindrome(string s, int);
 
 int main()
 {
     string s;
     cout << "Enter the string\n";
-    getline(cin, s);
+    getline(cin, s); // Input the string (can include spaces)
 
+    // Call the recursive palindrome function and print the result
     if (isPalindrome(s, 0))
     {
         cout << "It is a Palindrome\n";
@@ -19,23 +21,25 @@ int main()
     {
         cout << "Not a Palindrome\n";
     }
-    isalnum(s[0]);
 
-    return 0;
+    return 0; // Exit the program
 }
 
+// Recursive function to check if a string is a palindrome
 bool isPalindrome(string s, int i)
 {
-
+    // Base case: If we've checked up to the middle, it's a palindrome
     if (i >= s.length() / 2)
     {
         return true;
     }
 
+    // If characters at current positions don't match, it's not a palindrome
     if (s[i] != s[s.length() - i - 1])
     {
         return false;
     }
 
+    // Recursive call: check next pair of characters
     return isPalindrome(s, i + 1);
 }
