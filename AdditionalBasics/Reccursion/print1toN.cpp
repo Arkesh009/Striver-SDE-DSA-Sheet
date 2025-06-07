3ddedb686190cb067e6b7afbe268a000a740a34a
Arkesh009 <arkeshbhargava@gmail.com>
2025-06-07 12:00:00 +0530

    1toN

--- AdditionalBasics/Reccursion/print1toN.cpp
+++ AdditionalBasics/Reccursion/print1toN.cpp
@@ -3,23 +3,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Function declaration: prints numbers from x to n recursively
 void printN(int, int);
 
 int main()
 {
     int n;
-    cout << "Enter the cout\n";
-    cin >> n;
-    printN(n, 1);
+    cout << "Enter the count\n";
+    cin >> n; // Input the value of n
+    printN(n, 1); // Start printing from 1 to n
 
-    return 0;
+    return 0; // Exit the program
 }
 
+// Recursive function to print numbers from x to n
 void printN(int n, int x)
 {
     if (x <= n)
     {
-        cout << x << " ";
+        cout << x << " "; // Print the current number
+        printN(n, x + 1); // Recursive call to print the next number
     }
-    printN(n, x + 1);
+    // Base case: if x > n, recursion stops automatically
 }
