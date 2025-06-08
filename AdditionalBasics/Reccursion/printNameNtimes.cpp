8fb764534cc10a919be2a7565237c260878217b5
Arkesh009 <arkeshbhargava@gmail.com>
2025-06-08 12:00:00 +0530

    n times

--- AdditionalBasics/Reccursion/printNameNtimes.cpp
+++ AdditionalBasics/Reccursion/printNameNtimes.cpp
@@ -3,6 +3,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Function declaration: prints the name n times recursively, starting from x
 void printName(string name, int n, int x);
 
 int main()
@@ -11,19 +12,21 @@ int main()
     int n;
 
     cout << "Enter the name\n";
-    getline(cin, name);
+    getline(cin, name); // Input the name (can include spaces)
     cout << "Enter the number of times\n";
-    cin >> n;
-    printName(name, n, 1);
+    cin >> n; // Input the number of times to print the name
+    printName(name, n, 1); // Start printing from 1
 
-    return 0;
+    return 0; // Exit the program
 }
 
+// Recursive function to print the name n times
 void printName(string name, int n, int x)
 {
     if (x <= n)
     {
-        cout << name << " ";
+        cout << name << " "; // Print the name
+        printName(name, n, x + 1); // Recursive call to print the next time
     }
-    printName(name, n, x + 1);
+    // Base case: if x > n, recursion stops automatically
 }
