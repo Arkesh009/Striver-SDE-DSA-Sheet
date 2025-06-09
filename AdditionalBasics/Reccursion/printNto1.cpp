03ce76514b5967e3eb20651c21c8f7787c860014
Arkesh009 <arkeshbhargava@gmail.com>
2025-06-09 12:00:00 +0530

    n to 1

--- AdditionalBasics/Reccursion/printNto1.cpp
+++ AdditionalBasics/Reccursion/printNto1.cpp
@@ -3,23 +3,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Function declaration: prints numbers from n down to 1 recursively
 void printN(int);
 
 int main()
 {
     int n;
-    cout << "Enter the cout\n";
-    cin >> n;
-    printN(n);
+    cout << "Enter the count\n";
+    cin >> n; // Input the value of n
+    printN(n); // Start printing from n down to 1
 
-    return 0;
+    return 0; // Exit the program
 }
 
+// Recursive function to print numbers from n down to 1
 void printN(int n)
 {
     if (n > 0)
     {
-        cout << n << " ";
+        cout << n << " "; // Print the current number
+        printN(n - 1);    // Recursive call to print the next number
     }
-    printN(n - 1);
+    // Base case: if n <= 0, recursion stops automatically
 }
