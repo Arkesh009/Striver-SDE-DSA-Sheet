44234ff0c13abc1828f436ad592ac812f2d4dc47
Arkesh009 <arkeshbhargava@gmail.com>
2025-06-10 12:00:00 +0530

    reccursion

--- AdditionalBasics/Reccursion/ReccursionNtimes.cpp
+++ AdditionalBasics/Reccursion/ReccursionNtimes.cpp
@@ -1,8 +1,9 @@
-// Understanding Basic reccursion by printing N times
+// Understanding Basic recursion by printing N times
 
 #include <bits/stdc++.h>
 using namespace std;
 
+// Function declaration: prints character 'a' x times recursively
 void nPrints(int, char);
 
 int main()
@@ -10,20 +11,21 @@ int main()
     char a;
     int x;
     cout << "Enter the char you wanna print\n";
-    cin >> a;
+    cin >> a; // Input the character to print
     cout << "Enter the number of times you wanna print it\n";
-    cin >> x;
-    nPrints(x, a);
+    cin >> x; // Input the number of times to print the character
+    nPrints(x, a); // Call the recursive function
 
-    return 0;
+    return 0; // Exit the program
 }
 
+// Recursive function to print character 'a' x times
 void nPrints(int x, char a)
 {
     if (x == 0)
     {
-        return;
+        return; // Base case: stop recursion when x reaches 0
     }
-    cout << a << " ";
-    nPrints(x - 1, a); // calls the same function with (prev value - 1)
+    cout << a << " "; // Print the character
+    nPrints(x - 1, a); // Recursive call with decremented x
 }
