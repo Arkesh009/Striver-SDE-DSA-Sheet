7f1fa6e3d2aac5210d3e18d81748ad714f787618
Arkesh009 <arkeshbhargava@gmail.com>
2025-06-15 12:00:00 +0530

    sort colors

--- Arrays/sortColors.cpp
+++ Arrays/sortColors.cpp
@@ -3,10 +3,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Function to sort the array containing only 0s, 1s, and 2s
 void sortColors(vector<int> &nums)
 {
-    // sort(nums.begin(), nums.end());
-    int j = 0;
+    // Dutch National Flag algorithm (two-pass version)
+    int j = 0; // Pointer to place the next 0
+
+    // First pass: move all 0s to the front
     for (int i = 0; i < nums.size(); i++)
     {
         if (nums[i] == 0)
@@ -16,6 +19,7 @@ void sortColors(vector<int> &nums)
         }
     }
 
+    // Second pass: move all 1s after the 0s
     for (int i = j; i < nums.size(); i++)
     {
         if (nums[i] == 1)
