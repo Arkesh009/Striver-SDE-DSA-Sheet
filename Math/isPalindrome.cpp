be7139e1ad0c2a1a8362e2fa89cfe6c1b1dafc24
Arkesh009 <arkeshbhargava@gmail.com>
2025-06-17 12:00:00 +0530

    is Palindrome

--- Math/isPalindrome.cpp
+++ Math/isPalindrome.cpp
@@ -3,30 +3,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Function to check if an integer is a palindrome
 bool isPalindrome(int x)
 {
-
+    // Negative numbers and numbers ending with 0 (except 0 itself) are not palindromes
     if (x < 0 || (x % 10 == 0 && x != 0))
     {
         return false;
     }
 
     int reverse = 0;
-    int temp = x;
+    int temp = x; // Store the original value of x
+
+    // Reverse the digits of x
     while (x)
     {
-        int last = x % 10;
-        reverse = reverse * 10 + last;
-        x /= 10;
+        int last = x % 10;                // Get the last digit
+        reverse = reverse * 10 + last;    // Build the reversed number
+        x /= 10;                          // Remove the last digit
     }
 
+    // If the reversed number is equal to the original, it's a palindrome
     return temp == reverse;
 }
 
 int main()
 {
     int x;
-    cin >> x;
+    cin >> x; // Input the integer to check
 
     if (isPalindrome(x))
     {
@@ -37,5 +41,5 @@ int main()
         cout << "Its not\n";
     }
 
-    return 0;
+    return 0; // Exit the program
 }
