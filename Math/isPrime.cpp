dd7cccd0d7c8ef46b186d590430f577a91727a27
Arkesh009 <arkeshbhargava@gmail.com>
2025-06-18 12:00:00 +0530

    is Prime

--- Math/isPrime.cpp
+++ Math/isPrime.cpp
@@ -3,12 +3,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Function declaration: checks if x is a prime number
 bool isPrime(int x);
 
 int main()
 {
     int x;
-    cin >> x;
+    cin >> x; // Input the number to check
+
     // if function returns true
     if (isPrime(x))
     {
@@ -19,22 +21,24 @@ int main()
         cout << x << " is not a Prime number\n";
     }
 
-    return 0;
+    return 0; // Exit the program
 }
 
+// Function to check if a number is prime
 bool isPrime(int x)
 {
-    // if the input is 0 or 1
+    // if the input is 0 or 1, not prime
     if (x == 0 || x == 1)
     {
         return 0;
     }
-    for (int i = 2; i <= sqrt(x); i++) // 
+    // Check divisibility from 2 up to sqrt(x)
+    for (int i = 2; i <= sqrt(x); i++)
     {
-        if (x % i == 0)
+        if (x % i == 0) // If divisible, not prime
         {
             return 0;
         }
     }
-    return 1;
+    return 1; // If no divisors found, x is prime
 }
