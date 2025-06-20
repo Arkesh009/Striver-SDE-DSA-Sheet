6dcd2461cd4fe5a607f93c22e47e5c665d449a62
Arkesh009 <arkeshbhargava@gmail.com>
2025-06-20 12:00:00 +0530

    reverse int

--- Math/reverseInt.cpp
+++ Math/reverseInt.cpp
@@ -3,49 +3,49 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Function declaration: reverses the digits of an integer x
 int reverse(int x);
-// Method 1
+
+// Method 1: Arithmetic approach
 int main()
 {
     int x;
-    cin >> x;
-    cout << reverse(x);
+    cin >> x; // Input the integer to reverse
+    cout << reverse(x); // Output the reversed integer
 
-    return 0;
+    return 0; // Exit the program
 }
 
 int reverse(int x)
 {
+    long long int temp = 0; // Use long long to handle overflow during reversal
 
-    long long int temp = 0;
+    // Reverse the digits of x
     while (x)
     {
-        int last = x % 10;
-        temp = temp * 10 + last;
-        x /= 10;
+        int last = x % 10;           // Get the last digit
+        temp = temp * 10 + last;     // Append last digit to temp
+        x /= 10;                     // Remove the last digit
     }
 
+    // If the reversed number is out of 32-bit integer range, return 0
     if (temp > INT_MAX || temp < INT_MIN)
     {
         return 0;
     }
 
-    return temp;
+    return temp; // Return the reversed integer
 
-    // Method 2
+    // Method 2 (commented out): Using stringstream and string reversal
     // stringstream ss, ss2;
     // ss << x;
     // string s = ss.str();
     // reverse(s.begin(), s.end());
-
     // ss2 << s;
-
     // long long int a;
     // ss2 >> a;
-
     // if(a > INT_MAX || a < INT_MIN){
     //     return 0;
     // }
-
     // return a;
 }
